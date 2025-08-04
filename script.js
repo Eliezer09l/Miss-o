3d2e84187f5cfeeeb4996112c7fa193775f1b159
Eliezer09l <167527952+Eliezer09l@users.noreply.github.com>
2025-08-04 12:22:20 -0300

    missao

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const caixaAlternativas = document. querySelector(". caixa-alternativas");
 const caixaResultado = document. querySelector(". caixa-resultado");
 const textoResultado = document. querySelector(". texto-resultado");
 
+const perguntas=[
+    {
+        enunciado: Quantos participaram do mundia de clubes?,
+        alternativas:[
+            {texto: 32,
+                afirmação:resposta correta,
+                pontos:1
+            },
+            {
+                texto:16,
+                afirmação:resposta errada,
+                pontos:0
+            }
+            
+    ]
+    },
+]
+
+
