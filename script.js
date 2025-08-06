ad827e50e521211f935bc4a7ca6e34dc8aa74f40
Eliezer09l <167527952+Eliezer09l@users.noreply.github.com>
2025-08-06 09:46:07 -0300

    missão

--- script.js
+++ script.js
@@ -6,15 +6,79 @@ const textoResultado = document. querySelector(". texto-resultado");
 
 const perguntas=[
     {
-        enunciado: Quantos participaram do mundia de clubes?,
+        enunciado: "Quantos times participaram do Mundial de Clubes?",
         alternativas:[
-            {texto: 32,
-                afirmação:resposta correta,
+            {texto: "32",
+                afirmação:"Resposta correta",
                 pontos:1
             },
             {
-                texto:16,
-                afirmação:resposta errada,
+                texto:"16",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+            
+    ]
+    },
+
+{
+        enunciado: "Quantos times brasileiros participaram?",
+        alternativas:[
+            {texto: "4",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"2",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+            
+    ]
+    },
+
+{
+        enunciado: "Quem foi o artilheiro do Mundial de Clubes?",
+        alternativas:[
+            {texto: "Gonzalo Garcia",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Angél di Maria",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+            
+    ]
+    },
+
+{
+        enunciado: "Quem foi o Campeão?",
+        alternativas:[
+            {texto: "Chelsea",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"PSG",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+            
+    ]
+    },
+
+{
+        enunciado: "Quem foi o vice campeão?",
+        alternativas:[
+            {texto: "PSG",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Bayer",
+                afirmação:"Resposta errada",
                 pontos:0
             }
             
